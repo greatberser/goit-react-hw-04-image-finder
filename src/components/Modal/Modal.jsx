417ce5d40afe6c4bfb373a397d0b584f8e3efd81
Maks <maks135254@gmail.com>
2024-01-10 11:35:15 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,25 +9,25 @@ export const Modal = ({ imgSrc, imgAlt, onCloseModal }) => {
       }
     };
 
-    const handleClickOutside = (evt) => {
-      if (evt.target === evt.currentTarget) {
-        onCloseModal(); 
-      }
-    };
-
+    
     window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("click", handleClickOutside);
     disableScroll.on();
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("click", handleClickOutside);
       disableScroll.off();
     };
   }, [onCloseModal]);
 
+  const handleClickOutside = (evt) => {
+    if (evt.target === evt.currentTarget) {
+      onCloseModal(); 
+    }
+  };
+  
+
   return (
-    <div className="Overlay" onClick={onCloseModal}>
+    <div className="Overlay" onClick={handleClickOutside}>
       <div className="Modal">
         <img src={imgSrc} alt={imgAlt} />
       </div>
